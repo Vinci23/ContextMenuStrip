57b193c34be9f21d04f507db65fc0c04e2105449
Vinci23 <88689746+Vinci23@users.noreply.github.com>
2022-01-16 19:08:32 +0100

    Add files via upload

---
+++ ContextMenuStrip/ContextMenuStrip/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ContextMenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBoxLijevo = new System.Windows.Forms.TextBox();
+            this.textBoxDesno = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lIjevoNaDesnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desnoNaLijevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxLijevo
+            // 
+            this.textBoxLijevo.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBoxLijevo.Location = new System.Drawing.Point(120, 107);
+            this.textBoxLijevo.Name = "textBoxLijevo";
+            this.textBoxLijevo.Size = new System.Drawing.Size(161, 20);
+            this.textBoxLijevo.TabIndex = 0;
+            // 
+            // textBoxDesno
+            // 
+            this.textBoxDesno.ContextMenuStrip = this.contextMenuStrip1;
+            this.textBoxDesno.Location = new System.Drawing.Point(513, 107);
+            this.textBoxDesno.Name = "textBoxDesno";
+            this.textBoxDesno.Size = new System.Drawing.Size(174, 20);
+            this.textBoxDesno.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lIjevoNaDesnoToolStripMenuItem,
+            this.desnoNaLijevoToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(202, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // lIjevoNaDesnoToolStripMenuItem
+            // 
+            this.lIjevoNaDesnoToolStripMenuItem.Name = "lIjevoNaDesnoToolStripMenuItem";
+            this.lIjevoNaDesnoToolStripMenuItem.ShortcutKeyDisplayString = "";
+            this.lIjevoNaDesnoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.lIjevoNaDesnoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.lIjevoNaDesnoToolStripMenuItem.Text = "Lijevo na desno ";
+            this.lIjevoNaDesnoToolStripMenuItem.Click += new System.EventHandler(this.lIjevoNaDesnoToolStripMenuItem_Click);
+            // 
+            // desnoNaLijevoToolStripMenuItem
+            // 
+            this.desnoNaLijevoToolStripMenuItem.Name = "desnoNaLijevoToolStripMenuItem";
+            this.desnoNaLijevoToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.desnoNaLijevoToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.desnoNaLijevoToolStripMenuItem.Text = "Desno na lijevo ";
+            this.desnoNaLijevoToolStripMenuItem.Click += new System.EventHandler(this.desnoNaLijevoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxDesno);
+            this.Controls.Add(this.textBoxLijevo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLijevo;
+        private System.Windows.Forms.TextBox textBoxDesno;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lIjevoNaDesnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desnoNaLijevoToolStripMenuItem;
+    }
+}
+
---
+++ ContextMenuStrip/ContextMenuStrip/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContextMenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lIjevoNaDesnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBoxDesno.Text = textBoxLijevo.Text;
+        }
+
+        private void desnoNaLijevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBoxLijevo.Text = textBoxDesno.Text;
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
